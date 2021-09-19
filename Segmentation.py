f668e04c5bf29e3a19f160458b82c4ff81e77e23
Khalid Alreemy <46654145+khalidreemy@users.noreply.github.com>
2021-09-19 12:14:05 +0300

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -9,9 +9,11 @@ drive.mount('/content/drive')
 
 img = io.imread("drive/My Drive/Colab Notebooks/covid.jpg")
 plt.imshow(img,cmap="gray")
+#image available in /images folder covid.png
 
 img = img_as_float(img)
 plt.imshow(img,cmap="gray")
+
 
 sigma_est = np.mean(estimate_sigma(img,multichannel=True))
 
@@ -19,11 +21,14 @@ denoise = denoise_nl_means(img, h=1.15 * sigma_est, fast_mode=False, patch_size=
 
 plt.imshow(denoise,cmap="gray")
 
+
 denoise_ubyte = img_as_ubyte(denoise)
 
 plt.imshow(denoise_ubyte,cmap="gray")
+#image available in /images folder covid.png
 
 plt.hist(denoise_ubyte.flat, bins=100, range=(0,255))
+#image available in /images folder histogram.png
 
 seg1 = (denoise_ubyte <=10)
 seg2 = (denoise_ubyte > 10) & (denoise_ubyte <= 80)
@@ -34,7 +39,7 @@ seg6 = (denoise_ubyte > 160) & (denoise_ubyte <= 175)
 seg7 = (denoise_ubyte > 175) & (denoise_ubyte <= 210)
 seg8 = (denoise_ubyte > 210) 
 plt.imshow(seg7,cmap="gray")
-
+#image available in /images folder image1.png
 
 all_seg = np.zeros((denoise_ubyte.shape[0],denoise_ubyte.shape[1],3))
 all_seg[seg1] = (1,0,0)
@@ -47,4 +52,4 @@ all_seg[seg1] = (1,2,0)
 all_seg[seg1] = (2,0,0)
 
 plt.imshow(all_seg)
-
+#image available in /images folder image2.png
